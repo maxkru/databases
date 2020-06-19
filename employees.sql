d90a45594d46146b2ba7c9117f21673d23a8fdd1
maxkru <max_kru@hotmail.com>
2020-06-19 15:30:49 +0300

    Code beatification

--- employees.sql
+++ employees.sql
@@ -2,56 +2,59 @@ use employees;
 
 -- задание 1
 
-SELECT
-	d.dept_name, AVG(s.salary) as average_salary
+SELECT 
+    d.dept_name, AVG(s.salary) AS average_salary
 FROM
-	salaries AS s
-    INNER JOIN dept_emp ON s.emp_no = dept_emp.emp_no
-    INNER JOIN departments AS d ON dept_emp.dept_no=d.dept_no
+    salaries AS s
+        INNER JOIN
+    dept_emp ON s.emp_no = dept_emp.emp_no
+        INNER JOIN
+    departments AS d ON dept_emp.dept_no = d.dept_no
 WHERE
-	s.to_date = '9999-01-01' -- если хотим получить среднюю зарплату для текущих сотрудников
-group by d.dept_name;
+    s.to_date = '9999-01-01'
+GROUP BY d.dept_name;
 
-select * from dept_emp;
 
 -- задание 2
 
-SELECT
-	e.emp_no AS id, CONCAT(e.first_name, ' ', e.last_name) AS name, MAX(salary) AS max_salary
+SELECT 
+    e.emp_no AS id,
+    CONCAT(e.first_name, ' ', e.last_name) AS name,
+    MAX(salary) AS max_salary
 FROM
-	salaries AS s
-    INNER JOIN employees AS e ON e.emp_no = s.emp_no
-GROUP BY id, name
+    salaries AS s
+        INNER JOIN
+    employees AS e ON e.emp_no = s.emp_no
+GROUP BY id , name
 ORDER BY max_salary DESC;
 
 -- задание 3
 
-select * from titles;
+SELECT 
+    temp.emp_no
+FROM
+    (SELECT 
+        emp_no, MAX(salary) AS max_salary
+    FROM
+        salaries
+    GROUP BY emp_no
+    ORDER BY max_salary DESC
+    LIMIT 1) AS temp;
 
-SELECT temp.emp_no FROM (
-SELECT	
-	emp_no, MAX(salary) AS max_salary
-FROM salaries
-GROUP BY emp_no
-ORDER BY max_salary DESC
-LIMIT 1) AS temp;
-
-DELETE 
-	e.* FROM employees AS e
+DELETE e . * FROM employees AS e 
 WHERE
-	e.emp_no IN (
-		SELECT temp.emp_no FROM (
-			SELECT	
-				emp_no, MAX(salary) AS max_salary
-			FROM salaries
-			GROUP BY emp_no
-			ORDER BY max_salary DESC
-			LIMIT 1) AS temp
-		);
+    e.emp_no IN (SELECT 
+        temp.emp_no
+    FROM
+        (SELECT 
+            emp_no, MAX(salary) AS max_salary
+        FROM
+            salaries
+        GROUP BY emp_no
+        ORDER BY max_salary DESC
+        LIMIT 1) AS temp);
         
 -- задание 4
-
-SELECT * FROM dept_emp;
 
 -- выполняется примерно за 0.70 с (в среднем)
 SELECT
@@ -67,8 +70,7 @@ FROM
 			d_e.to_date = '9999-01-01' -- только текущие сотрудники
 		GROUP BY
 			d_e.dept_no
-	) AS t 
-    ON t.dept_no = d.dept_no
+	) AS t ON t.dept_no = d.dept_no
 ORDER BY
 	t.number_of_employees DESC;
 
@@ -88,15 +90,17 @@ ORDER BY
 
 -- задание 5
 
-SELECT
-	d.dept_name, COUNT(*) AS number_of_employees, SUM(salary) AS money_received_by_department
+SELECT 
+    d.dept_name,
+    COUNT(*) AS number_of_employees,
+    SUM(salary) AS money_received_by_department
 FROM
-	dept_emp AS d_e
-    INNER JOIN departments AS d ON d_e.dept_no = d.dept_no
-    INNER JOIN salaries AS s ON s.emp_no = d_e.emp_no
+    dept_emp AS d_e
+        INNER JOIN
+    departments AS d ON d_e.dept_no = d.dept_no
+        INNER JOIN
+    salaries AS s ON s.emp_no = d_e.emp_no
 WHERE
-	s.to_date = '9999-01-01' -- только текущие сотрудники
-GROUP BY
-	d.dept_name
-ORDER BY
-	number_of_employees DESC;
+    s.to_date = '9999-01-01'
+GROUP BY d.dept_name
+ORDER BY number_of_employees DESC;
--- geodata.sql
+++ geodata.sql
@@ -2,18 +2,26 @@ use geodata;
 
 -- задание 1
 
-select
-	_countries.title as country_name, _regions.title as region_name
-from _cities
-	INNER JOIN _regions ON _cities.region_id = _regions.id
-    INNER JOIN _countries ON _cities.country_id = _countries.id
-WHERE _cities.title = 'Октябрьский';
+SELECT 
+    _countries.title AS country_name,
+    _regions.title AS region_name
+FROM
+    _cities
+        INNER JOIN
+    _regions ON _cities.region_id = _regions.id
+        INNER JOIN
+    _countries ON _cities.country_id = _countries.id
+WHERE
+    _cities.title = 'Октябрьский';
 
 -- задание 2
 
-select
-	c.title as city_name
-from _cities as c
-	INNER JOIN _regions as r ON c.region_id = r.id
-WHERE r.title = 'Московская область';
+SELECT 
+    c.title AS city_name
+FROM
+    _cities AS c
+        INNER JOIN
+    _regions AS r ON c.region_id = r.id
+WHERE
+    r.title = 'Московская область';
 
